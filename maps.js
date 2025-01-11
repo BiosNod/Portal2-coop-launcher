695fde2015eff6ee59f34189ea002371b5714f6a
biosnod <biosnod@gmail.com>
2025-01-12 01:23:28 +0300

    Add translation for en|ru, fix maps keys, redesign, show cmd preview before execute

--- maps.js
+++ maps.js
@@ -10,62 +10,62 @@ let coopMainChapters =
             '02 Buttons | 02 Кнопки - mp_coop_race_2',
             '03 Lasers | 03 Лазеры - mp_coop_laser_2',
             '04 Rat Maze | 04 Крысиный лабиринт - mp_coop_rat_maze',
-            '05 Laser Crusher - mp_coop_laser_crusher | 05 Лазерная дробилка - mp_coop_laser_crusher',
-            '06 Behind the scenes - mp_coop_teambts | 06 За кулисами - mp_coop_teambts'
+            '05 Laser Crusher | 05 Лазерная дробилка - mp_coop_laser_crusher',
+            '06 Behind the scenes | 06 За кулисами - mp_coop_teambts'
         ],
 
         'Chapter 2: Mass and Velocity | Глава 2: Масса и скорость': [
             '01 Flings | 01 Ленты - mp_coop_fling_3',
             '02 Infinifling | 02 Без препятствий - mp_coop_infinifling_train',
             '03 Team Retrieval | 03 Поиск команды - mp_coop_come_along',
-            '04 Vertical Flings | 04 Vertical Flings - mp_coop_fling_1',
+            '04 Vertical Flings | 04 Вертикальные броски - mp_coop_fling_1',
             '05 Catapults | 05 Катапульты - mp_coop_catapult_1',
-            '06 Multifling | 06 Multifling - mp_coop_multifling_1',
-            '07 Fling Crushers | 07 Fling Crushers - mp_coop_fling_crushers',
-            '08 Industrial Fan | 08 Industrial Fan - mp_coop_fan'
+            '06 Multifling | 06 Мультифлинг - mp_coop_multifling_1',
+            '07 Fling Crushers | 07 Дробилки для метания - mp_coop_fling_crushers',
+            '08 Industrial Fan | 08 Промышленный вентилятор - mp_coop_fan'
         ],
 
         'Chapter 3: Hard-Light Surfaces | Глава 3: Мосты плотного света': [
             '01 Cooperative Bridges | 01 Кооперативные мосты - mp_coop_wall_intro',
             '02 Bridge Swap | 02 Обмен мостами - mp_coop_wall_2',
-            '03 Fling Block | 03 Fling Block - mp_coop_catapult_wall_intro',
+            '03 Fling Block | 03 Бросить блок - mp_coop_catapult_wall_intro',
             '04 Catapult Block | 04 Блок катапульты - mp_coop_wall_block',
-            '05 Bridge Fling | 05 Bridge Fling - mp_coop_catapult_2',
+            '05 Bridge Fling | 05 Бросок моста - mp_coop_catapult_2',
             '06 Turret Walls | 06 Стены из турелей - mp_coop_turret_walls',
             '07 Turret Assassin | 07 Убийца турелей - mp_coop_turret_ball',
             '08 Bridge Testing | 08 Проверка моста - mp_coop_wall_5'
         ],
 
         'Chapter 4: Excursion Funnels | Глава 4: Экскурсионные воронки': [
-            '01 Cooperative Funnels | 01 Cooperative Funnels - mp_coop_tbeam_redirect',
-            '02 Funnel Drill | 02 Funnel Drill - mp_coop_tbeam_drill',
-            '03 Funnel Catch | 03 Funnel Catch - mp_coop_tbeam_catch_grind_1',
-            '04 Funnel Laser | 04 Funnel Laser - mp_coop_tbeam_laser_1',
+            '01 Cooperative Funnels | 01 Кооперативные воронки - mp_coop_tbeam_redirect',
+            '02 Funnel Drill | 02 Воронкообразное сверло - mp_coop_tbeam_drill',
+            '03 Funnel Catch | 03 Воронкообразный улов - mp_coop_tbeam_catch_grind_1',
+            '04 Funnel Laser | 04 Воронкообразный лазер - mp_coop_tbeam_laser_1',
             '05 Cooperative Polarity | 05 Совместная полярность - mp_coop_tbeam_polarity',
-            '06 Funnel Hop | 06 Funnel Hop - mp_coop_tbeam_polarity2',
+            '06 Funnel Hop | 06 Воронка хоп - mp_coop_tbeam_polarity2',
             '07 Advanced Polarity | 07 Продвинутая полярность - mp_coop_tbeam_polarity3',
-            '08 Funnel Maze | 08 Funnel Maze - mp_coop_tbeam_maze',
+            '08 Funnel Maze | 08 Лабиринт-воронка - mp_coop_tbeam_maze',
             '09 Turret Warehouse | 09 Склад турелей - mp_coop_tbeam_end'
         ],
 
         'Chapter 5: Mobility Gels | Глава 5: Ускоряющие гели': [
-            '01 Repulsion Jumps | 01 Repulsion Jumps - mp_coop_paint_come_along',
+            '01 Repulsion Jumps | 01 Прыжки с отталкиванием - mp_coop_paint_come_along',
             '02 Double Bounce | 02 Двойной отскок - mp_coop_paint_redirect',
             '03 Bridge Repulsion | 03 Мостовое отталкивание - mp_coop_paint_bridge',
             '04 Wall Repulsion | 04 Настенное отталкивание - mp_coop_paint_walljumps',
-            '05 Propulsion Crushers | 05 Propulsion Crushers - mp_coop_paint_speed_fling',
-            '06 Turret Ninja | 06 Turret Ninja - mp_coop_paint_red_racer',
-            '07 Propulsion Retrieval | 07 Propulsion Retrieval - mp_coop_paint_speed_catch',
-            '08 Vault Entrance | 08 Vault Entrance - mp_coop_paint_longjump_intro'
+            '05 Propulsion Crushers | 05 Движущиеся дробилки - mp_coop_paint_speed_fling',
+            '06 Turret Ninja | 06 Турель ниндзя - mp_coop_paint_red_racer',
+            '07 Propulsion Retrieval | 07 Извлечение тяги - mp_coop_paint_speed_catch',
+            '08 Vault Entrance | 08 Вход в хранилище - mp_coop_paint_longjump_intro'
         ],
 
         'Additional chapter: Art Therapy (DLC) | Дополнительная глава: Арт-терапия (DLC)': [
             '01 Separation | 01 Разделение - mp_coop_separation_1',
             '02 Triple Axis | 02 Тройная ось - mp_coop_tripleaxis',
-            '03 Catapult Catch | 03 Catapult Catch - mp_coop_catapult_catch',
+            '03 Catapult Catch | 03 Катапультируйся и лови - mp_coop_catapult_catch',
             '04 Bridge Gels | 04 Мостовые гели - mp_coop_2paints_1bridge',
             '05 Maintenance | 05 Обслуживание - mp_coop_paint_conversion',
-            '06 Bridge Catch | 06 Bridge Catch - mp_coop_bridge_catch',
+            '06 Bridge Catch | 06 Ловля на мосту - mp_coop_bridge_catch',
             '07 Double Lift | 07 Двухместный лифт - mp_coop_laser_tbeam',
             '08 Gel Maze | 08 Гелевой лабиринт - mp_coop_paint_rat_maze',
             '09 Crazier Box | 09 Безумная коробка - mp_coop_paint_crazy_box',
